012b0df979b3169cad7de232254fd45df386932e
Alesialoco <125814881+Alesialoco@users.noreply.github.com>
2024-11-09 10:38:28 +0300

    Add files via upload

--- DynamicArr.h
+++ DynamicArr.h
@@ -1,21 +1,28 @@
 #pragma once
+#include <stddef.h>
 
+template <class T>
 class DynamicArr {
 	static const size_t initialCapacity_ = 2;	// only one for all objects of DynamicArr
 	static const double coeff_;					// same as above
 
 	size_t size_ = 0;
 	size_t capacity_ = initialCapacity_;
-	double* arr_;
+	T* arr_;
 
 public:
 	DynamicArr();
-	DynamicArr(size_t size, double element = 0);
-	DynamicArr(const DynamicArr& rhs);
+	explicit DynamicArr(size_t size, T elem = 0);
+	DynamicArr(const DynamicArr<T>& rhs);
+	DynamicArr(DynamicArr<T>&& rhs);
 	~DynamicArr();
 	size_t size() const;
 	size_t capacity() const;
-	void push_back(double elem);
-	DynamicArr& operator=(DynamicArr other);
-	double operator[](const int index);
+	void push_back(T elem);
+	T& operator[](size_t index);
+	const T& operator[](size_t index) const;
+	DynamicArr<T>& operator=(const DynamicArr& rhs);
+	DynamicArr& operator=(DynamicArr<T>&& rhs);
 };
+
+void fun();
